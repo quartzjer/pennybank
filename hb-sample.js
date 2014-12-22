a1505157fe72991516be2c720405b51b6813d068
Jeremie Miller <jeremie@jabber.org>
2014-12-22 08:39:40 -0700

    experimenting/learning more of the bitcoinjs-lib api

--- hb-sample.js
+++ hb-sample.js
@@ -1,3 +1,4 @@
+var crypto = require('crypto');
 var bitcoin = require("bitcoinjs-lib")
 var helloblock = require('helloblock-js')({
   network: 'testnet'
@@ -24,19 +25,31 @@ helloblock.faucet.withdraw(address, 2e4, function(err) {
     unspents = unspents.filter(function(unspent) { return unspent.value > 1e4 })
 
     // use the oldest unspent
-    var unspent = unspents.pop()
+    var unspent = unspents.pop();
+//    helloblock.transactions.get(unspent.txHash, function(err, res, transaction) {
+//      if (err) return done(err)
+//        console.log("unspent",unspent,transaction);
+//    });
 
     var tx = new bitcoin.TransactionBuilder()
 
-    var data = new Buffer('cafedeadbeef', 'hex')
-    var dataScript = bitcoin.scripts.nullDataOutput(data)
+//    var redeemScript = bitcoin.scripts.multisigOutput(2, pubKeys) // 2 of 3
+//    var scriptPubKey = bitcoin.scripts.scriptHashOutput(redeemScript.getHash())
+//    var address = bitcoin.Address.fromOutputScript(scriptPubKey, bitcoin.networks.testnet).toString()
+
+//    var data = new Buffer('cafedeadbeef', 'hex')
+//    var dataScript = bitcoin.Script.fromChunks([bitcoin.opcodes.OP_RETURN, data]);
+    
+    var secret = crypto.randomBytes(4);
+    var hash = crypto.createHash('sha256').update(secret).digest();
+    var hashScript = bitcoin.Script.fromChunks([bitcoin.opcodes.OP_HASH256, hash, bitcoin.opcodes.OP_EQUALVERIFY]);
 
     tx.addInput(unspent.txHash, unspent.index)
-    tx.addOutput(dataScript, 1000)
+    tx.addOutput(hashScript, 2000)
     tx.sign(0, key)
 
     var hex = tx.build().toHex();
-    console.log("HEX",hex);
+    console.log("TX-HEX",hex);
     helloblock.transactions.propagate(hex, function(err) {
       if (err) return done(err)
 
@@ -46,12 +59,36 @@ helloblock.faucet.withdraw(address, 2e4, function(err) {
 
         var transaction = transactions[0]
         var output = transaction.outputs[0]
-        var dataScript2 = bitcoin.Script.fromHex(output.scriptPubKey)
-        var data2 = dataScript2.chunks[1]
+        var hashScript2 = bitcoin.Script.fromHex(output.scriptPubKey)
+        var hash2 = hashScript2.chunks[1]
 
-        console.log("TEST",dataScript,data);
+        console.log(hash.toString('hex'),hash2.toString('hex'));
+//        console.log(transaction);
 
-        done()
+        // build another transaction to spend the P2H1
+        var tx2 = new bitcoin.TransactionBuilder();
+        var data = new Buffer('cafedeadbeef', 'hex')
+        var dataScript = bitcoin.Script.fromChunks([bitcoin.opcodes.OP_RETURN, data]);
+        tx2.addInput(transaction.txHash, 0)
+        tx2.addOutput(dataScript, 1000)
+        tx2.sign(0, key)
+// TODO trying to figure out bitcoin-lib's internals to add the hash to the tx to validate the P2H1 script
+//        tx2.tx.setInputScript(0, hash);
+
+        var hex2 = tx2.build().toHex();
+//        var hex2 = tx2.tx.toHex();
+        console.log("TX2-HEX",hex2);
+
+        helloblock.transactions.propagate(hex2, function(err) {
+          if (err) return done(err)
+
+          // check that the message was propagated
+          helloblock.transactions.get(hex2, function(err, res, transaction) {
+            if (err) return done(err);
+            console.log("DONE",transaction);
+            done()
+          })
+        })
       })
     })
   })
