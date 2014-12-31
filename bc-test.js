505cfcce50a7bb3e2e4b1df12f18b8cf9ccd81c7
Jeremie Miller <jeremie@jabber.org>
2014-12-31 10:55:53 -0600

    simply the bc test to match the spec

--- bc-test.js
+++ bc-test.js
@@ -1,55 +1,27 @@
 var crypto = require('crypto');
 var bitcore = require('bitcore');
 
-var secret = new Buffer('pennybank');
-var p2h1_in = bitcore.Script().add(secret);
-console.log(p2h1_in);
+var A_secret = crypto.randomBytes(16);
+var B_secret = crypto.randomBytes(16);
 
-var hash1 = crypto.createHash('sha256').update(secret).digest();
-var hash2 = crypto.createHash('sha256').update(hash1).digest();
+function hash160(buf)
+{
+  var sha256 = crypto.createHash('sha256').update(buf).digest();
+  return crypto.createHash('ripemd160').update(sha256).digest();
+}
 
-var p2h1 = bitcore.Script().add('OP_HASH256').add(hash2).add('OP_EQUALVERIFY');
-console.log(p2h1);
+var A_hash = hash160(A_secret);
+var B_hash = hash160(B_secret);
 
-verified = bitcore.Script.Interpreter().verify(p2h1_in, p2h1);
-console.log(verified);
+var P2CM = bitcore.Script()
+  .add('OP_HASH160')
+  .add(A_hash)
+  .add('OP_EQUALVERIFY')
+  .add('OP_HASH160')
+  .add(B_hash)
+  .add('OP_EQUAL');
 
-var int = bitcore.Script.Interpreter();
-var verified = int.verify(p2h1_in, p2h1);
-console.log(verified, int.errstr);
+var P2CM_IN = bitcore.Script().add(B_secret).add(A_secret);
+var verified = bitcore.Script.Interpreter().verify(P2CM_IN, P2CM);
+console.log(P2CM_IN,P2CM,verified);
 
-var p2sh = p2h1.toScriptHashOut();
-console.log(p2sh,bitcore.Script.Interpreter().verify(p2h1, p2sh));
-
-var test = bitcore.Script().add(secret).add('OP_HASH256').add(hash2).add('OP_EQUALVERIFY');
-console.log(test,bitcore.Script.Interpreter().verify(bitcore.Script().add('OP_NOP'), p2h1));
-
-var i = bitcore.Script.Interpreter();
-i.set({script:test});
-console.log(i.evaluate());
-
-var p2h1 = bitcore.Script().add('OP_HASH256').add(hash2).add('OP_EQUAL');
-console.log(p2h1);
-
-verified = bitcore.Script.Interpreter().verify(p2h1_in, p2h1);
-console.log(verified);
-
-////////
-
-var a_secret = new Buffer('pennybank');
-var a_p2h1_in = bitcore.Script().add(a_secret);
-var b_secret = new Buffer('pennybank');
-var b_p2h1_in = bitcore.Script().add(b_secret);
-console.log(a_p2h1_in,b_p2h1_in);
-
-var a_hash1 = crypto.createHash('sha256').update(a_secret).digest();
-var a_hash2 = crypto.createHash('sha256').update(a_hash1).digest();
-
-var b_hash1 = crypto.createHash('sha256').update(b_secret).digest();
-var b_hash2 = crypto.createHash('sha256').update(b_hash1).digest();
-
-var x_p2h1 = bitcore.Script().add('OP_HASH256').add(a_hash2).add('OP_EQUALVERIFY').add('OP_HASH256').add(b_hash2).add('OP_EQUAL');
-console.log(x_p2h1);
-
-verified = bitcore.Script.Interpreter().verify(bitcore.Script().add(b_secret).add(a_secret), x_p2h1);
-console.log(verified);
