a3ee8471419d88285c5859039d884187e70f2307
Jeremie Miller <jeremie@jabber.org>
2015-01-10 08:25:21 -0700

    update bitcore and experiment w/ multisig

--- bc-test.js
+++ bc-test.js
@@ -25,3 +25,33 @@ var P2CM_IN = bitcore.Script().add(B_secret).add(A_secret);
 var verified = bitcore.Script.Interpreter().verify(P2CM_IN, P2CM);
 console.log(P2CM_IN,P2CM,verified);
 
+// test multisig
+var privateKey1 = new bitcore.PrivateKey('KwF9LjRraetZuEjR8VqEq539z137LW5anYDUnVK11vM3mNMHTWb4');
+var privateKey2 = new bitcore.PrivateKey('L4PqnaPTCkYhAqH3YQmefjxQP6zRcF4EJbdGqR8v6adtG9XSsadY');
+var publicKey1 = privateKey1.publicKey;
+var publicKey2 = privateKey2.publicKey;
+var P2SHScript = new bitcore.Script.buildMultisigOut([publicKey1, publicKey2], 1);
+console.log(P2SHScript);
+var P2SHFund = P2SHScript.toScriptHashOut();
+console.log(P2SHFund);
+var address = P2SHFund.toAddress();
+console.log(address);
+
+
+// first we create a transaction
+var uxto = {
+  address: address,
+  txId: '66e64ef8a3b384164b78453fa8c8194de9a473ba14f89485a0e433699daec140',
+  outputIndex: 0,
+  script: P2SHFund,
+  satoshis: 100000
+};
+var tx = new bitcore.Transaction().from(uxto,[publicKey1],1).to(P2SHFund.toAddress(), 100000).sign(privateKey1);
+//console.log(tx.inputs[0].getSignatures(tx, privateKey1, 0));
+var signature = tx.getSignatures(privateKey1)[0].signature.toBuffer();
+console.log(signature);
+
+var P2SH_IN = bitcore.Script().add(signature);
+var verified = bitcore.Script.Interpreter().verify(P2SH_IN, P2SHScript);
+console.log(P2SH_IN,verified);
+
