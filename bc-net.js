030ef70395eaaa70cfebec8369e025dd521a38ce
Jeremie Miller <jeremie@jabber.org>
2015-01-17 09:45:35 -0700

    some updates to match the recent changes in bitcore

--- bc-net.js
+++ bc-net.js
@@ -1,5 +1,7 @@
 var crypto = require('crypto');
 var bitcore = require('bitcore');
+var explorers = require('bitcore-explorers');
+var insight = new explorers.Insight(bitcore.Networks.testnet);
 
 // testnet
 var privateKey = bitcore.PrivateKey({
@@ -23,7 +25,7 @@ getUTXO(address, function(utxo){
   
   var tx = new bitcore.Transaction()
     .from(utxo)
-    .to(P2SHFund.toAddress(), 1000)
+    .to(P2SHFund.toAddress(), 10000)
     .change(address)
     .sign(privateKey); 
 
@@ -33,11 +35,11 @@ getUTXO(address, function(utxo){
     console.log("funded to",id);
 
     var tx2 = new bitcore.Transaction()
-      .from({txId:id, outputIndex:0, inputIndex:0, satoshis:1000, script:P2SHFund.toString()}, [publicKey1, publicKey2], 1)
-      .to(address, 1000)
+      .from({txId:id, outputIndex:0, inputIndex:0, satoshis:10000, script:P2SHFund.toString()}, [publicKey1, publicKey2], 1)
+      .to(address, 10000)
       .sign(privateKey2); 
 
-    console.log("tx2",tx2);
+    console.log("tx2",tx2.serialize());
     broadcast(tx2, function(id2){
       console.log("funded back to",id2);
     });
@@ -48,7 +50,6 @@ getUTXO(address, function(utxo){
 
 function broadcast(tx, done)
 {
-  var insight = new bitcore.transport.explorers.Insight(bitcore.Networks.testnet);
   insight.broadcast(tx, function(err, id) {
     if (err) {
       console.log("insight broadcast err",err);
@@ -60,7 +61,6 @@ function broadcast(tx, done)
 
 function getUTXO(address, done)
 {
-  var insight = new bitcore.transport.explorers.Insight(bitcore.Networks.testnet);
   insight.getUnspentUtxos(address, function(err, utxos) {
     if (err) {
       console.log("insight utxo err",err);
--- bc-p2cm.js
+++ bc-p2cm.js
@@ -1,5 +1,9 @@
 var crypto = require('crypto');
 var bitcore = require('bitcore');
+var Sighash = require('./node_modules/bitcore/lib/transaction/sighash')
+var explorers = require('bitcore-explorers');
+var insight = new explorers.Insight(bitcore.Networks.testnet);
+
 
 // testnet
 var privateKey = bitcore.PrivateKey({
@@ -40,7 +44,7 @@ getUTXO(address, function(utxo){
   
   var tx = new bitcore.Transaction()
     .from(utxo)
-    .to(P2SHFund.toAddress(), 1000)
+    .to(P2SHFund.toAddress(), 10000)
     .change(address)
     .sign(privateKey); 
 
@@ -50,17 +54,18 @@ getUTXO(address, function(utxo){
     console.log("funded to",id);
 
     var tx2 = new bitcore.Transaction()
-      .from({txId:id, outputIndex:0, inputIndex:0, satoshis:1000, script:P2SHFund.toString()}, [publicKey1, publicKey2], 1)
-      .to(address, 1000)
+      .from({txId:id, outputIndex:0, inputIndex:0, satoshis:10000, script:P2SHFund.toString()}, [publicKey1, publicKey2], 1)
+      .to(address, 10000)
       .sign(privateKey2);
 
 //    console.log('tx2 input',tx2.inputs[0]);
 
     // work around hard-wired multisig to get the signature (TODO make a real input class for P2CM)
-    var signature = bitcore.Transaction.Sighash.sign(tx2, privateKey2, 1, 0, P2CMScript).toBuffer();
+//    var signature = Sighash.sign(tx2, privateKey2, 1, 0, P2CMScript).toBuffer();
 //    console.log("tx2 signed",signature);
           
     // use the real script
+/*
     var s = new bitcore.Script();
     s.add('OP_0');
     s.add(signature);
@@ -70,7 +75,8 @@ getUTXO(address, function(utxo){
     console.log("INPUT",s.toString());
     tx2.inputs[0].setScript(s);
     console.log(tx2.toJSON());
-
+*/
+    console.log("CHECK",tx2.serialize())
     broadcast(tx2, function(id2){
       console.log("funded back to",id2);
     });
@@ -81,7 +87,6 @@ getUTXO(address, function(utxo){
 
 function broadcast(tx, done)
 {
-  var insight = new bitcore.transport.explorers.Insight(bitcore.Networks.testnet);
   insight.broadcast(tx, function(err, id) {
     if (err) {
       console.log("insight broadcast err",err);
@@ -93,7 +98,6 @@ function broadcast(tx, done)
 
 function getUTXO(address, done)
 {
-  var insight = new bitcore.transport.explorers.Insight(bitcore.Networks.testnet);
   insight.getUnspentUtxos(address, function(err, utxos) {
     if (err) {
       console.log("insight utxo err",err);
