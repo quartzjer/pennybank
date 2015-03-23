82f01f759fa5419e8683140452fa084d386cd5d6
Jeremie Miller <jeremie@jabber.org>
2015-03-22 18:07:14 -0600

    expand the alicebob flow

--- alicebob.js
+++ alicebob.js
@@ -1,29 +1,55 @@
-var libchallenge = require('./challenge.js');
+var libset = require('./set.js');
 
-// WIP, just pseudo-code yet of one exchange to start, in reality two exchanges are happening both ways simultaneously
+// example code showing flow of an Alice / Bob exchange
 
-/*
+var Alice = {who:'Alice'};
+var Bob = {who:'Bob'};
 
-// bob advertises required penny bank details to alice, the confidence (number of sets), and quantity and cost of each microtx
+// Alice generates a new set
+Alice.set = libset.generate(1000);
 
-// alice generates sets
-var alice_sets = libchallenge.Challenge(transactions, bits, quantity);
+// Alice uses the shareable parts in an open
+Alice.open = {pence:Alice.set.pence, N:Alice.set.N, nonce:Alice.set.nonce};
 
-// alice sends bob the hashes to initiate a penny bank verification
-alice.send(bob, alice_sets.challenges);
+// Alice sends open to Bob
 
-// bob chooses one of the sets to use at random and tells alice
-var bob_alice_selected = alice_challenges[random];
-bob.send(alice, bob_alice_selected);
+// Bob generates a set and open response
+Bob.set = libset.generate(Alice.open.N);
+Bob.open = {pence:Bob.set.pence, N:Bob.set.N, nonce:Bob.set.nonce};
 
-// alice sends bob all the secrets of the other sets, except the selected
-var alice_bob_secrets = alice_sets.secrets[bob_alice_selected]; // remove it
-alice.send(bob, alice_sets.secrets);
+// Bob selects one pence randomly from Alice's open
+var random = Math.floor(Math.random()*100);
+Bob.Alice = {N:Alice.open.N, nonce:Alice.open.nonce};
+Bob.Alice.ID = Object.keys(Alice.open.pence)[random];
+Bob.Alice.pN = Alice.open.pence[Bob.Alice.ID];
+Bob.open.ID = Bob.Alice.ID; // to send back to Alice
 
-// bob verifies the secrets
-bob.verify(alice_sets.secrets, transactions, bits);
+// Bob sends the open response to Alice
 
-// now bob has a set of proof of work hashes of a known size
-// TODO, create the PB transaction using the P2H2 of bob_alice_selected 
+// Alice selects one pence randomly from Bob's open
+var random = Math.floor(Math.random()*100);
+Alice.Bob = {N:Bob.open.N, nonce:Bob.open.nonce};
+Alice.Bob.ID = Object.keys(Bob.open.pence)[random];
+Alice.Bob.pN = Bob.open.pence[Alice.Bob.ID];
 
-*/
+// Alice generates confirmation for Bob
+Alice.secret = Alice.set.secrets[Bob.open.ID];
+delete Alice.set.pence[Bob.open.ID];
+Alice.opened = {ID:Alice.Bob.ID};
+Alice.opened.secrets = Object.keys(Alice.set.pence).map(function(id){return Alice.set.secrets[id].p0;});
+
+// Alice sends opened to Bob
+
+// Bob verifies secrets
+libset.verify(Bob.Alice, Alice.opened.secrets);
+
+// Bob creates final response
+Bob.secret = Bob.set.secrets[Alice.opened.ID];
+delete Bob.set.pence[Alice.opened.ID];
+Bob.opened = {};
+Bob.opened.secrets = Object.keys(Bob.set.pence).map(function(id){return Bob.set.secrets[id].p0;});
+
+// Bob sends opened to Alice
+
+// Alice verifies, fully negotiated mutual pence
+libset.verify(Alice.Bob, Bob.opened.secrets);
